9e477dcd4f7a92e6ae7c3f2c06c1b5b775853e79
Dan Zajdband <dan@getmango.com>
2015-05-01 16:51:36 -0300

    agregado pcs

--- tp2/src/pca/pca.cpp
+++ tp2/src/pca/pca.cpp
@@ -1,18 +1,45 @@
 
 #include "pca.h"
 
-/*void calcularPca(vector<entrada> &etiquetados, vector<entrada> &sinEtiquetar, string &salida, int cantidadDeVecinosMasCercanos)
+void calcularPca(vector<entrada> &etiquetados, vector<entrada> &sinEtiquetar, string &salida, int cantidadDeVecinosMasCercanos)
 {
-  
-}*/
+  vector< vector<double> > covarianza = vector< vector<double> >(etiquetados[0].vect->dim, vector<double>(etiquetados[0].vect->dim, 0));
+  matrizDeCovarianza(etiquetados, covarianza);
 
-void matrizDeCovarianza(vector<entrada> &etiquetados)
-{
-  vector<double> media = vector<double>(etiquetados[0].vect->dim, 0);
-  media(etiquetados, media);
+  vector<double> potencias = vector<double>(1);
+  vector<double> aux = vector<double>(0);
+  double norma = 0;
+
+  // iteraciones del metodo de potencias
+  for(int i = 0; i < 10; i++) {
+    norma = 0;  
+
+    for(int j = 0; j < covarianza.size(); j++) {
+      aux[j] = 0;
+      for(int k = 0; k < potencias.size(); k++) {
+        aux[j] += covarianza[j][k] * potencias[k];
+      }
+      norma += aux[j] * aux[j];
+    }
+
+    norma = sqrt(norma);
+
+    for(int k = 0; k < potencias.size(); k++) {
+      potencias[k] = aux[k] / norma;
+    }
+
+  }
+
 }
 
-void media(vector<entrada> &v, vector<double> &media)
+void matrizDeCovarianza(vector<entrada> &etiquetados, vector< vector<double> > &covarianza)
+{
+  vector<double> media = vector<double>(etiquetados[0].vect->dim, 0);
+  matMedia(etiquetados, media);
+  matCovarianza(etiquetados, media, covarianza);
+}
+
+void matMedia(vector<entrada> &v, vector<double> &media)
 {
   double aux = 0.0;
 
@@ -23,5 +50,19 @@ void media(vector<entrada> &v, vector<double> &media)
     }
     media[i] = aux / v.size(); 
     cout << "media " << i << " " << media[i] << endl;
+  }
+}
+
+
+void matCovarianza(vector<entrada> &v, vector<double> &media, vector< vector<double> > &covar)
+{
+  for(int j = 0 ; j < v[0].vect->dim; j++) {
+    for(int k = 0 ; k < v[0].vect->dim; k++) {
+      double aux = .0;
+      for(int i = 0 ; i < v.size(); i++) {
+        aux += (v[i].vect->get(j) - media[j])*(v[i].vect->get(k) - media[k]);
+      }
+      covar[j][k] = aux / (v.size() - 1);
+    }
   }
 }
--- tp2/src/pca/pca.h
+++ tp2/src/pca/pca.h
@@ -2,8 +2,11 @@
 #define _PCA_H
 
 #include <iostream>
+#include <cmath.h>
 #include "../manejoEntrada/entrada.h"
 
-void media(vector<entrada> &v, vector<double> &media);
+void matMedia(vector<entrada> &v, vector<double> &media);
+void matCovarianza(vector<entrada> &v, vector<double> &media, vector< vector<double> > &covar);
+void matrizDeCovarianza(vector<entrada> &etiquetados, vector< vector<double> > &covarianza);
 
 #endif
