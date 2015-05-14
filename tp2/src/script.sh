f160aa1b5acd232ff173ea6f8c6869d9c181bf08
FranckN <franconegri2004@hotmail.com>
2015-05-14 16:23:45 -0300

    Idem

--- tp2/src/script.sh
+++ tp2/src/script.sh
@@ -3,5 +3,5 @@ for vecinos in 1 .. 10
 do
 	sed -i -e "1d" tests/test1.in
 	echo '/data/ ' $vecinos ' 10 5' | cat - tests/test1.in > temp && mv temp tests/test1.in
-    ./tp tests/test1.in output 1 
+    ./tp tests/test1.in output 1 >> log
 done
