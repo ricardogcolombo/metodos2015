cce3d6f22670f99a5d470747eb983798654666a8
FranckN <franconegri2004@hotmail.com>
2015-06-02 12:39:42 -0300

    Arreglado el sherman morrison(Ahora, posta posta)

--- tp1/src/SMAlt/SM.cpp
+++ tp1/src/SMAlt/SM.cpp
@@ -1,14 +1,11 @@
 #include "SM.h"
 #define INFINITO 10000000000
 using namespace std;
-double** encontrarMatrizInversa(MatrizB *m);
 double* armarVectorU(int i, MatrizB *mat);
 double* armarVectorVT(int j, MatrizB *mat);
-double calcularConstanteLoca(double* VT, double **inv, double * U, int dim);
 vector<sanguijuelaDiscretizada*> *discretizarSangs(vector<sanguijuela*>* sanguijuelas, double intervalo, int cantidadDeColumnas, int cantidadDeFilas);
-double* calcularMatrizDeShermanMorrison(double *vecU, double* vecVT, MatrizB* A, double* b, int dim);
+double* calcularMatrizDeShermanMorrison(double *vecU, double* vecVT, MatrizB* L, MatrizB* U , double* b, int dim);
 double *copiarB(double *b, int tam);
-double *matrizPorVector( double** inversa, double *vector, int dimencion);
 double *salvacionSM(instancia *ins, double *b) {
 	int sanguijuelaParaEliminar = -1;
 	sanguijuela* a_eliminar = NULL;
@@ -19,10 +16,8 @@ double *salvacionSM(instancia *ins, double *b) {
 	int posPuntoCritico = ins->m->getP() / 2 + (ins->m->getP()) * (ins->m->getP() / 2);
 	vector<sanguijuelaDiscretizada*> *sangDiscretizadas = discretizarSangs(ins->sanguijuelas, ins->intervalo, ins->m->getP(), ins->m->getP());
 	int dimencion = ins->m->getN();
-
-	//Sacamos Y
-	
-
+	MatrizB* U = ins->m;
+	MatrizB* L = DescompLU(U);
 	for (int i = 0; i < sangDiscretizadas->size() ; i++) {
 		//Obtengo una copia de B y modifico solo la fila donde esta la sanguijuela
 		double *nuevoB = copiarB(b, dimencion);
@@ -31,8 +26,7 @@ double *salvacionSM(instancia *ins, double *b) {
 		double * vecU = armarVectorU((*sangDiscretizadas)[i]->x, ins->m);
 		double * vecVT = armarVectorVT((*sangDiscretizadas)[i]->y, ins->m);
 		//Ahora utilizo la ecuación de sherman morrison
-		MatrizB* A2 = ins->m->copy();
-		double* respuesta = calcularMatrizDeShermanMorrison(vecU, vecVT, A2, b, dimencion);
+		double* respuesta = calcularMatrizDeShermanMorrison(vecU, vecVT, L, U, nuevoB, dimencion);
 		//Si es la mejor respuesta obtenida hasta el momento, la guardo.
 		punto_critico_local = respuesta[posPuntoCritico];
 		if (punto_critico_local < punto_critico_global) {
@@ -60,50 +54,6 @@ double *copiarB(double *b, int tam) {
 	}
 	return nuevoB;
 }
-double *matrizPorVector(double** inversa, double *vector, int dimencion) {
-	double *respuesta = new double[dimencion];
-	for (int j = 0; j < dimencion; j++) {
-		respuesta[j] = 0;
-		for (int w = 0; w < dimencion; w++) {
-			respuesta[j] += (vector[w]) * (inversa[j][w]);
-		}
-	}
-	return respuesta;
-}
-double *vectorTporMatriz(double* vector, double** matriz, int dimencion) {
-	double *respuesta = new double[dimencion];
-	for (int j = 0; j < dimencion; j++) {
-		respuesta[j] = 0;
-		for (int w = 0; w < dimencion; w++) {
-			respuesta[j] += vector[w] * matriz[w][j];
-		}
-	}
-	return respuesta;
-}
-double **vectorPorVectorT(double* AaLaMenosUnoPorU, double* VTporAalaMenosUno, int dimencion) {
-	double** matrix = new double*[dimencion];
-	for (int w = 0; w < dimencion; w++) {
-		matrix[w] = new double[dimencion];
-		for (int j = 0; j < dimencion; j++) {
-			matrix[w][j] = AaLaMenosUnoPorU[w] * VTporAalaMenosUno[j];
-		}
-	}
-	return matrix;
-}
-void dividirMatrizPorCte(double** matrix, double cte, int dimencion) {
-	for (int w = 0; w < dimencion; w++) {
-		for (int j = 0; j < dimencion; j++) {
-			matrix[w][j] = matrix[w][j] / cte;
-		}
-	}
-}
-void restaMatrices(double **matriz1, double **matriz2, int dimencion) {
-	for (int w = 0; w < dimencion; w++) {
-		for (int j = 0; j < dimencion; j++) {
-			matriz2[w][j] = matriz1[w][j] - matriz2[w][j];
-		}
-	}
-}
 double vectorTporVector(double *vectorT, double* vect, int dim) {
 	double respuesta = 0;
 	for (int i = 0; i < dim; i++) {
@@ -111,94 +61,27 @@ double vectorTporVector(double *vectorT, double* vect, int dim) {
 	}
 	return respuesta;
 }
-double* calcularMatrizDeShermanMorrison(double *vecU, double* vecVT, MatrizB* A, double* b, int dim) {
+double* calcularMatrizDeShermanMorrison(double *vecVT, double* vecU, MatrizB* L, MatrizB* U , double* b, int dim) {
 	//double *b;
-
-	MatrizB *L = DescompLU(A);
 	double *aux = foward_substitution(L, b);
-	double *y = backward_substitution(A, aux);
+	double *y = backward_substitution(U, aux);
 
 	aux = foward_substitution(L, vecU);
-	double *z = backward_substitution(A, aux);
+	double *z = backward_substitution(U, aux);
 
-	double n1 = vectorTporVector(vecVT,y,dim);
-	double divisor = vectorTporVector(vecVT,z,dim);
-	divisor++;
-	n1= n1/divisor;
-	for(int i = 0; i < dim; i++)
-		z[i] = y[i] - z[i]*n1;
-
+	double n1 = vectorTporVector(vecVT, y, dim);
+	double divisor = vectorTporVector(vecVT, z, dim);
+	divisor = divisor + 1.0;
+	n1 = (double) n1 / (double) divisor;
+	for (int i = 0; i < dim; i++) {
+		z[i] = y[i] - (z[i] * n1);
+	}
 	return z;
-}
-double calcularConstanteLoca(double* VT, double** inv, double * U, int dim) {
-	double *invPorU = matrizPorVector(inv, U, dim);
-	double respuesta = vectorTporVector(VT , invPorU, dim);
-	respuesta += 1;
-	delete[] invPorU;
-	return respuesta;
 }
 sanguijuelaDiscretizada::sanguijuelaDiscretizada(int _x, int _y, double _temp) {
 	x = _x;
 	y = _y;
 	temp = _temp;
-}
-double *generarCanonico(int i, int dim) {
-	double* vectorCanonico = new double[dim];
-	for (int j = 0; j < dim; j++) {
-		vectorCanonico[j] = 0;
-	}
-	vectorCanonico[i] = 1;
-	return vectorCanonico;
-}
-
-void test(double **mat1, double **mat2, int dim) {
-	double rta;
-	for (int i = 0; i < dim; i++) {
-		for (int j = 0; j < dim; j++) {
-			for (int w = 0; w < dim; w++) {
-				rta += mat1[i][w] * mat2[w][j];
-			}
-			cout << rta << "\t\t\t\t";
-			rta = 0;
-		}
-		cout << endl;
-	}
-
-}
-
-double** encontrarMatrizInversa(MatrizB *m) {
-	int dim = m->getN();
-
-	double **matrixAUX = new double*[dim];
-	for (int i = 0; i < dim; i++) {
-		matrixAUX[i] = new double[dim];
-		for (int j = 0; j < dim; j++) {
-			matrixAUX[i][j] = m->getVal(i, j);
-		}
-	}
-
-	MatrizB *U = m;
-	MatrizB *L = DescompLU(U);
-	double** inversa = new double*[dim];
-	for (int i = 0; i < dim; i++) {
-		inversa[i] = new double[dim];
-	}
-	for (int i = 0; i < dim; i++) {
-		double* vectorCanonico = generarCanonico(i, dim);
-		double* y = foward_substitution(L, vectorCanonico);
-		delete[] vectorCanonico;
-		double *respuesta = backward_substitution(U, y);
-		delete[] y;
-		for (int j = 0; j < dim; j++) {
-			inversa[j][i] = respuesta[j];
-		}
-		delete[] respuesta;
-	}
-	delete L;
-
-	//test(matrixAUX, inversa, dim);
-
-	return inversa;
 }
 double* armarVectorU(int i, MatrizB *mat) {
 	int tam = mat->getN();
@@ -225,29 +108,35 @@ double* armarVectorVT(int j, MatrizB *mat) {
 	return vectorVT;
 }
 vector<sanguijuelaDiscretizada*> *discretizarSangs(vector<sanguijuela*>* sanguijuelas, double intervalo, int cantidadDeColumnas, int cantidadDeFilas) {
+	int contador;
 	vector<sanguijuelaDiscretizada*> *sanguDisc = new vector<sanguijuelaDiscretizada*>();
 	for (int i = 0; i < sanguijuelas->size(); i++) {
+		contador = 0;
 		sanguijuela *s = (*sanguijuelas)[i];
+
 		int x_comienzo = round((s->x - 1 * (s->radio + 1)) / intervalo);
 		int y_comienzo = round((s->y - 1 * (s->radio + 1)) / intervalo);
+
 		int x_fin = round((s->x + 1 * (s->radio + 1)) / intervalo);
 		int y_fin = round((s->y + 1 * (s->radio + 1)) / intervalo);
-		if (s->radio < intervalo) {
-			for (int j = x_comienzo ; j <= x_fin; j++) {
-				for (int w = y_comienzo; w <= y_fin; w++) {
-					if (pow(j * intervalo - s->x , 2.0) + pow(w * intervalo - s->y, 2.0) <= pow(s->radio, 2.0)) {
-						int CoordenadaEnLaMatriz = (w + (j * cantidadDeColumnas));
-						if (w > 0 && w < cantidadDeColumnas - 1) {
-							if (j > 0 && j < cantidadDeFilas - 1) {
-								if (CoordenadaEnLaMatriz % cantidadDeColumnas != 0 && CoordenadaEnLaMatriz % cantidadDeColumnas != cantidadDeColumnas - 1 ) {
-									sanguDisc->push_back(new sanguijuelaDiscretizada(CoordenadaEnLaMatriz, CoordenadaEnLaMatriz, (*sanguijuelas)[i]->temperatura));
-								}
+
+		for (int j = x_comienzo ; j <= x_fin; j++) {
+			for (int w = y_comienzo; w <= y_fin; w++) {
+				if (pow(j * intervalo - s->x , 2.0) + pow(w * intervalo - s->y, 2.0) <= pow(s->radio, 2.0)) {
+					int CoordenadaEnLaMatriz = (w + (j * cantidadDeColumnas));
+					if (w > 0 && w < cantidadDeColumnas - 1) {
+						if (j > 0 && j < cantidadDeFilas - 1) {
+							if (CoordenadaEnLaMatriz % cantidadDeColumnas != 0 && CoordenadaEnLaMatriz % cantidadDeColumnas != cantidadDeColumnas - 1 ) {
+								sanguDisc->push_back(new sanguijuelaDiscretizada(CoordenadaEnLaMatriz, CoordenadaEnLaMatriz, (*sanguijuelas)[i]->temperatura));
+								contador++;
 							}
 						}
 					}
 				}
 			}
 		}
+		if(contador > 1)
+			cout << "Error, LA SANGUIJUELA NO ES DISCRETIZALE" << endl;
 	}
 	return sanguDisc;
 }
