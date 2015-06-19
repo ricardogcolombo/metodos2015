dd744aae1830d3d61b33d453bb274f10e2741cc6
FranckN <franconegri2004@hotmail.com>
2015-06-19 13:38:50 -0300

    Agregadas condiciones de vorde al algoritmo

--- tp3/src/ventanas.cpp
+++ tp3/src/ventanas.cpp
@@ -7,7 +7,6 @@ void dividirEnVentanas(Mat *image, Mat *imageRes, int k) {
 	cout << image->rows << " " << image->cols << endl;
 	for (int i = 0; i < image->rows - tamanioVentana; i += tamanioVentana - 1)
 		for (int j = 0; j < image->cols - tamanioVentana; j += tamanioVentana - 1) {
-			cout << i << " " << j << endl;
 			Mat ventana = (*image)(Rect(j, i, tamanioVentana, tamanioVentana));
 			int resRows = (ventana.rows - 1) * k + ventana.rows;
 			int resCols = (ventana.cols - 1) * k + ventana.cols;
@@ -15,6 +14,33 @@ void dividirEnVentanas(Mat *image, Mat *imageRes, int k) {
 			bicubic(&ventana, &ventanaAumentada, k);
 			copy(imageRes, &ventanaAumentada, i * (k + 1), j * (k + 1));
 		}
+
+	//Estas dos pasadas son para agarrar bien los bordes
+	for (int i = 0; i < image->rows - tamanioVentana; i += tamanioVentana - 1){
+		cout << i << endl;
+		Mat ventana = (*image)(Rect(image->cols -tamanioVentana, i, tamanioVentana, tamanioVentana));
+		int resRows = (ventana.rows - 1) * k + ventana.rows;
+		int resCols = (ventana.cols - 1) * k + ventana.cols;
+		Mat ventanaAumentada(resRows, resCols, DataType<uchar>::type);
+		bicubic(&ventana, &ventanaAumentada, k);
+		copy(imageRes, &ventanaAumentada, i * (k + 1), (image->cols - tamanioVentana) * (k + 1));
+	}
+
+	for (int i = 0; i < image->cols - tamanioVentana; i += tamanioVentana - 1){
+		Mat ventana = (*image)(Rect(i,image->rows - tamanioVentana, tamanioVentana, tamanioVentana));
+		int resRows = (ventana.rows - 1) * k + ventana.rows;
+		int resCols = (ventana.cols - 1) * k + ventana.cols;
+		Mat ventanaAumentada(resRows, resCols, DataType<uchar>::type);
+		bicubic(&ventana, &ventanaAumentada, k);
+		copy(imageRes, &ventanaAumentada,( image->rows - tamanioVentana)* (k + 1), i * (k + 1));
+	}
+
+	Mat ventana = (*image)(Rect(image->cols - tamanioVentana,image->rows - tamanioVentana, tamanioVentana, tamanioVentana));
+	int resRows = (ventana.rows - 1) * k + ventana.rows;
+	int resCols = (ventana.cols - 1) * k + ventana.cols;
+	Mat ventanaAumentada(resRows, resCols, DataType<uchar>::type);
+	bicubic(&ventana, &ventanaAumentada, k);
+	copy(imageRes, &ventanaAumentada,( image->rows - tamanioVentana)* (k + 1),(image->cols - tamanioVentana)* (k + 1));
 }
 
 
@@ -23,5 +49,4 @@ void copy(Mat *dest, Mat *src, int fila, int columna) {
 		for (int j = 0; j < src-> cols; j++ ) {
 			dest->at<uchar>(fila + i, columna + j) = src->at<uchar>(i, j);
 		}
-
 }
