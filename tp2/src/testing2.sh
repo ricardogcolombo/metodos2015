b280c97177614d93b9953caad13fb564eed28551
FranckN <franconegri2004@hotmail.com>
2015-07-08 17:49:06 -0300

    ooo

--- tp2/src/testing2.sh
+++ tp2/src/testing2.sh
@@ -3,5 +3,5 @@ for i in `seq 1 50`; do
 	echo "corriendo" $i
 	VAR=". 1 $i 10"
 	sed -i "1s/.*/$VAR/" ./TESTING2/test.in
-	./tp TESTING2/test.in lol 1 2>> TESTING2/tiempos.txt
+	./tp TESTING2/test.in lol2 1 2>> TESTING2/tiempos.txt
 done
--- tp2/src/testing3.sh
+++ tp2/src/testing3.sh
@@ -3,5 +3,5 @@ for i in `seq 1 50`; do
 	echo "corriendo" $i
 	VAR=". 3 $i 10"
 	sed -i "1s/.*/$VAR/" ./TESTING3/test.in
-	./tp TESTING3/test.in lol 1 2>> TESTING3/tiempos.txt
+	./tp TESTING3/test.in lol3 1 2>> TESTING3/tiempos.txt
 done
