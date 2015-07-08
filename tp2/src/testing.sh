80cd1ffdcaf30519bdb453e4b455e7b958a4fc1c
FranckN <franconegri2004@hotmail.com>
2015-07-08 15:05:49 -0300

    Cambiado script para terminar testing

--- tp2/src/testing.sh
+++ tp2/src/testing.sh
@@ -4,6 +4,6 @@ python metnum.py
 rm TESTING/*.txt
 for i in `seq 1 $1`; do
 	echo "corriendo" $i
-	./TESTING/tester 3 $i 5
+	./TESTING/tester $i 14 5
 	./tp TESTING/test.txt lol 1 2>> TESTING/tiempos.txt
 done
