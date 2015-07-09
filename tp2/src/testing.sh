71ceb4b781e077afc9caf653f93d050efbdace77
negri franco <franconegri2004@hotmail.com>
2015-07-09 11:09:36 -0300

    testing, testing everyweere

--- tp2/src/testing.sh
+++ tp2/src/testing.sh
@@ -4,7 +4,7 @@ python metnum.py
 rm TESTING/*.txt
 for i in `seq 1 $1`; do
 	echo "corriendo" $i
-	VAR=". $1 14 10"
+	VAR=". $i 14 10"
 	sed -i "1s/.*/$VAR/" ./TESTING/test.in
-	./tp TESTING/test.in lol 1 2>> TESTING/tiempos.txt
+	./tp TESTING/test.in lol 0 2>> TESTING/tiempos.txt
 done
