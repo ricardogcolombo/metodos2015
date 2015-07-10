1a0521598e082640ede3fed32e0c94e558ff65af
FranckN <franconegri2004@hotmail.com>
2015-07-10 15:54:29 -0300

    Limpiado el repo

--- tp2/src/testing2.sh
+++
@@ -1,7 +0,0 @@
-rm TESTING2/*.txt
-for i in `seq 1 50`; do
-	echo "corriendo" $i
-	VAR=". 1 $i 10"
-	sed -i "1s/.*/$VAR/" ./TESTING2/test.in
-	./tp TESTING2/test.in lol2 1 2>> TESTING2/tiempos.txt
-done
--- tp2/src/testing3.sh
+++
@@ -1,7 +0,0 @@
-rm TESTING3/*.txt
-for i in `seq 1 50`; do
-	echo "corriendo" $i
-	VAR=". 3 $i 10"
-	sed -i "1s/.*/$VAR/" ./TESTING3/test.in
-	./tp TESTING3/test.in lol3 1 2>> TESTING3/tiempos.txt
-done
